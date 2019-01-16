ca1c3d58d8fdb054bf19c83a2a9aa4384a21e0a8
Tahara <hydoro-3181@itstudio.co>
2019-01-16 15:22:45 +0900

    SASS導入とREADME編集

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# mobile-page
+# vue-cliを使ったレスポンシブページ
 
 > A Vue.js project
 
-## Build Setup
+## ファイルをダンロードして使う手順
 
 ``` bash
 # install dependencies
@@ -17,5 +17,58 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
+
+
+
+## 最初から作成方法
+
+### vue-cli のインストール
+
+```
+npm install -g vue-cli
+```
+
+### プロジェクトを作成
+
+```
+vue init webpack my-project
+```
+
+プロジェクト作成は色々聞かれます。
+
+YかNで答えます。
+
+### サーバー起動
+
+```
+cd my-project
+npm run dev
+```
+
+### ビルド
+
+```
+npm run build
+```
+
+
+
+### SASSの導入
+
+srcに移動して下記コマンド実施
+
+```
+npm install sass-loader node-sass --save-dev
+```
+
+styleタグには以下のようにlang="scss"を付ける。
+
+```
+<style lang="scss">
+/* write SASS! */
+</style>
+```
+
+
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/GrobalNav_sp.vue
+++ src/components/GrobalNav_sp.vue
@@ -49,7 +49,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 .button{
   display: block;
   width: 50px;
@@ -61,36 +61,38 @@ export default {
   appearance: none;
   cursor: pointer;
   outline: none;
+  &.is-open{
+    .top{
+      transform: rotate(45deg);
+    }
+    .middle{
+      opacity: 0;
+    }
+    .bottom{
+      transform: rotate(-45deg);
+    }
+  }
+  span{
+    display: block;
+    width: 20px;
+    height: 1px;
+    margin: auto;
+    background-color: #000;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transition: .5s ease;
+    &.top{
+      transform: translateY(-6px);
+    }
+    &.bottom{
+      transform: translateY(6px);
+    }
+  }
 }
 
-.button.is-open .top{
-  transform: rotate(45deg);
-}
-.button.is-open .middle{
-  opacity: 0;
-}
-.button.is-open .bottom{
-  transform: rotate(-45deg);
-}
-span{
-  display: block;
-  width: 20px;
-  height: 1px;
-  margin: auto;
-  background-color: #000;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  transition: .5s ease;
-}
-span.top{
-  transform: translateY(-6px);
-  }
-span.bottom{
-  transform: translateY(6px);
-}
 .nav{
   width: 100%;
   height: 100%;
@@ -100,38 +102,44 @@ span.bottom{
   background-color: #39AF78;
   overflow-y: scroll;
   z-index: 99;
+  &.v-enter,
+  &.v-leave-to{
+    opacity: 0;
+  }
+  &.v-enter-to,
+  &.v-leave{
+    opacity: 1;
+  }
+
+  &.v-enter-active,
+  &.v-leave-active{
+    transition: opacity 1s ease;
+  }
+
+
+  &__body{
+    padding: 40px;
+  }
+
+  &__lst{
+    list-style: none;
+    padding-left:0;
+  }
+
+  &__item{
+    font-size: 24px;
+    line-height: 1.5;
+    font-weight: 700;
+    & ~ &{
+      margin-top: 8px;
+    }
+    a{
+      color: #fff;
+      padding: 8px 0;
+    }
+  }
 }
-.v-enter,
-.v-leave-to{
-  opacity: 0;
-}
-.v-enter-to,
-.v-leave{
-  opacity: 1;
-}
-.v-enter-active,
-.v-leave-active{
-  transition: opacity 1s ease;
-}
-.nav__body{
-  padding: 40px;
-}
-.nav__lst{
-  list-style: none;
-  padding-left:0;
-}
-.nav__item{
-  font-size: 24px;
-  line-height: 1.5;
-  font-weight: 700;
-}
-.nav ~ .nav{
-  margin-top: 8px;
-}
-a{
-  color: #fff;
-  padding: 8px 0;
-}
+
 .text{
   position: absolute;
   top: 0;
